340569c7a5e61d2034dbbebb1728d82206a58532
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-02-19 12:54:20 +0200

    Update and rename swap_elements.cc to 1_swap_elements.cc

--- basic/array/1_swap_elements.cc
+++ basic/array/1_swap_elements.cc
@@ -11,10 +11,18 @@
 void swapElements(int array[], int arrayLen, int idx1, int idx2){
     int lastIdx = arrayLen - 1;
     int firstIdx = 0;
-    //Don't touch the array if the indexes are not in range.
+    //Max sure the given indexes are in range.
     if(idx1 >= firstIdx && idx2 >= firstIdx && idx1 <= lastIdx && idx2 <= lastIdx){
         int saveIdx1Element = array[idx1]
         array[idx1] = array[idx2];
         array[idx2] = saveIdx1Element;
+        // Another option which I personally preferes but is
+        // less common when teaching:
+        // Saving phase:
+        // int elemInIdx1 = array[idx1] 
+        // int elemInIdx2 = array[idx2]
+        // Swapping phase:
+        // array[idx1] = elemInIdx2;
+        // array[idx2] = elemInIdx1;
     }
 }

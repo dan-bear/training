8bd1598aa4718826a46a4ea359a350e73f66ecfc
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-02-19 13:14:01 +0200

    Update 2_shift_one_left.cc

--- basic/array/2_shift_one_left.cc
+++ basic/array/2_shift_one_left.cc
@@ -1,7 +1,14 @@
 /**
+ * The function gets an array and its length and shift 
+ * all its element one index to the left in a cyclic
+ * manner (i.e. the 0 element is shifted to the len - 1 
+ * index).
+ * For example: array = {1, 2, 3, 4}
+ * After calling shiftOneLeft the array changes to: {2, 3, 4, 1}.
+ *
  * Note: a more efficient solution would be to use
- *       mem copy, but at this stage this subject
- *       is not covered.
+ *       mem copy, but it is not covered in a basic
+ *       course.
  */
 void shiftOneLeft(int array[], int arrayLen){
     int nextElement = array[0];

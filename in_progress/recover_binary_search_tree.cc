1938d747fe29ad158d0431e4ef22a2b5a2d36299
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-02-20 11:53:06 +0200

    Update recover_binary_search_tree.cc

--- in_progress/recover_binary_search_tree.cc
+++ in_progress/recover_binary_search_tree.cc
@@ -1,6 +1,36 @@
 /**
  * Explain why and how the inorder works.
  * https://leetcode.com/problems/recover-binary-search-tree/
+ * 
+ * The idea of this algorithm goes as follows:
+ * let's assume that node[i] and node[j] are not in
+ * place and node[i].val < node[j].val. 
+ * It means that:
+ * 1: The subtree of node[i] is a valid BST.
+ * 2: The subtree of node[j] is a valid BST.
+ * 3: The subtree excepted from cutting node[i]'s and node[j]'s
+ *    subtree is also a valid BST.
+ * So, a sequenec of an inorder traverseal output can be separated
+ * to (at most) 3 monotinically increasing runs, when the values
+ * of node[i] and node[j] will be the separating elements between
+ * the monotonically increasing runs.
+ * For example, the tree:
+ *          8   
+ *     4           6 
+ *  2    12     10      14
+ * 1 3  5  7   9  11  13  15
+ *  
+ * switchig 12 and 6 fixes the tree, an inorder traversal leads to:
+ * 1 2 3 4 5 12 7 8 6 9 10 11 13 14 15
+ * 1 < 2 < 3 < 4 < 5 < 12 is the first runn.
+ * 7 < 8 is the second run.
+ * 6 < 9 < 10 < 11 < 13 < 14 < 15 is the third run.
+ * The first element that is not in place is the first one to
+ * not satisfy in_order_arr[i] < in_order_arr[i+1] (last element
+ * of the first run).
+ * The second element that is not in place it the one satisfying:
+ * in_order_array[i-1] > in_order_array[i] (the first element of
+ * the third run).
  */
 
 class Solution {

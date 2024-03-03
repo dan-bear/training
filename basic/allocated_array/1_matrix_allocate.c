3f078b2ce4ca5263d8812a9fbca80414cd71c11d
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-03-03 15:22:14 +0200

    Create 1_matrix_allocate.c

---
+++ basic/allocated_array/1_matrix_allocate.c
@@ -0,0 +1,17 @@
+/**
+ * Allocates a matrix of integers with given row dimension
+ * and col dimension.
+ * @note You don't need to initialize the memory value, just
+ * allocate it.
+ * @param rowDim the rows dimension of the matrix.
+ * @param colDim the columns dimension of the matrix.
+ * @return the allocated matrix.
+ */
+int** allocateIntMatrix(int rowDim, int colDim)
+{
+    int** matrix = (int**)malloc(rowDim * sizeof(int*));
+    for (int rowIdx = 0; rowIdx < rowDim; rowIdx++) {
+        matrix[rowIdx] = (int*)malloc(colDim * sizeof(int));
+    }
+    return matrix;
+}

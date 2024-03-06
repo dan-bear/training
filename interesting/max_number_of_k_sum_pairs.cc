56bd9451701e9a917b06fd756ae089e85317415a
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-03-06 07:02:16 +0200

    Update and rename max_number_of_k_sum_pairs.cc to max_number_of_k_sum_pairs.cc

--- interesting/max_number_of_k_sum_pairs.cc
+++ interesting/max_number_of_k_sum_pairs.cc
@@ -6,34 +6,15 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        // unordered_map<int, int> histogram;
-        // fillHistogram(nums, histogram);
-        // int kPairs = countPairs(histogram, k);
-        // return kPairs;
-        return otherSolution(nums, k);
+        unordered_map<int, int> histogram;
+        fillHistogram(nums, histogram);
+        int kPairs = countPairs(histogram, k);
+        return kPairs;
+        ///Another solution, that runs a little faster.
+        //return singleFunctionSolution(nums, k);
     }
 private:
-    int otherSolution(const vector<int>& nums, int k){
-        unordered_map<int, int> histogram;
-        int opCount = 0;
-        for(int idx = 0; idx < nums.size(); idx++){
-            if(histogram.find(k - nums[idx]) != histogram.end()){
-                if(histogram[k - nums[idx]] > 0){
-                    histogram[k - nums[idx]]--;    
-                    opCount++;
-                }
-            }else{
-                if(histogram.find(nums[idx]) != histogram.end()){
-                    histogram[nums[idx]]++;
-                }else{
-                    histogram[nums[idx]] = 1;
-                }
-            }
-        }
-        return opCount;
-    }
-
-
+    
     void fillHistogram(const vector<int>& nums, unordered_map<int, int>& histogram){
         for(int elem: nums){
             /// could have also used histogram.count(elem) == 0
@@ -57,6 +38,26 @@ private:
             count = 0;//avoid double counting.
         }
         return pairsCount;
+    }
+
+    int singleFunctionSolution(const vector<int>& nums, int k){
+        unordered_map<int, int> histogram;
+        int opCount = 0;
+        for(int idx = 0; idx < nums.size(); idx++){
+            if(histogram.count(k - nums[idx]) && histogram[k - nums[idx]] > 0){
+                ///pair with nums[idx].
+                histogram[k - nums[idx]]--;    
+                opCount++;
+            }else{
+                ///cannot pair nums[idx] now, but maybe later.
+                if(histogram.count(nums[idx])){
+                    histogram[nums[idx]]++;
+                }else{
+                    histogram[nums[idx]] = 1;
+                }
+            }
+        }
+        return opCount;
     }
 };
 

111ca9f9503d30d75b9446ebf8f240417bffd348
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-05-07 17:44:53 +0300

    Create koko_eating_banana.cc

---
+++ koko_eating_banana.cc
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode.com/problems/koko-eating-bananas
+ * #binary_search
+ */
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int minEatRate = 1;
+        int maxEatRate = ranges::max(piles);
+        int res = maxEatRate;
+        while(minEatRate <= maxEatRate){
+            int midEatRate = (minEatRate + maxEatRate) / 2;
+            if(canEatAll(piles, h, midEatRate)){
+                res = midEatRate;
+                maxEatRate = midEatRate - 1;
+            }else{
+                minEatRate = midEatRate + 1;
+            }
+        }
+        return res;
+    }
+
+private:
+    bool canEatAll(const vector<int>& piles, int hours, int eatRate){
+        int timeToEat = 0;
+        for(int pileIdx = piles.size() - 1;
+            pileIdx >= 0 && timeToEat <= hours;
+            pileIdx--){
+            
+            timeToEat += ceil(piles[pileIdx], eatRate);
+        }
+        return timeToEat <= hours;
+    }
+
+    int ceil(int val, int divisor){
+        int ceilAddition = val % divisor == 0 ? 0 : 1;
+        int res = (val / divisor) + ceilAddition;
+        return res;
+    }
+
+};

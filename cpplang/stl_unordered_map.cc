fc02efdcd7d514c44f96c59f7cec76c0f8740207
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-05-11 16:18:18 +0300

    Update stl_unordered_map.cc

--- cpplang/stl_unordered_map.cc
+++ cpplang/stl_unordered_map.cc
@@ -1,1 +1,21 @@
+///-----------------------------------------------------------------------------
+using namespace std;
+int main(){
+    unordered_map<int, bool> histo1; //amoritezed O(1) histo using hash table.
+    histo1[3] = true;
+    histo1[31] = true;
+    histo1[314] = true;
+    histo1[3141] = true;
+    unordered_map<int, bool> histo2= {{31415, true},
+                                      {314159, true},
+                                      {3141592, true}};
+    if(histo1.find(271828) == histo1.end()){
+        histo1[271828] = true;
+    }
 
+    for(const pair<int, bool>& mapPair : histo1){
+        cout << mapPair.first << " " << mapPair.second << "\n";
+    }
+
+    return 0;
+}

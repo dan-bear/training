39e9b0ce468cac417075b8945d92de317298e4fb
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-05-11 18:26:38 +0300

    Create const_ptr_backward_rule.cc

---
+++ cpplang/const_ptr_backward_rule.cc
@@ -0,0 +1,16 @@
+/** 
+ * Const and pointers, the backward / spiral rule.
+ * https://stackoverflow.com/questions/1143262
+ */
+int main(){
+    int mutVal = 2;
+    const int immutVal = 3; 
+    int* const pcMutVal = &mutVal; // const pointer to int.
+    int const* pImmutVal = &immutVal; // pointer to const int.
+    int const * const pcUmmutVal = &ummutVal //const pointer to const int.
+    return 0;
+}
+
+
+
+

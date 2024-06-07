29b5f232b12d1da7559121717a733bc8648f92b6
dan-bear <121826152+dan-bear@users.noreply.github.com>
2024-06-07 08:03:52 +0300

    Update 3_longest_common_subsequence.cc

--- dp/3_longest_common_subsequence.cc
+++ dp/3_longest_common_subsequence.cc
@@ -4,9 +4,11 @@
  */
 class Solution {
 public:
-    ///dp[i][j] if text1[i] == text2[j]:
-    ///             dp[i][j] = dp[i-1][j-1] + 1
-    ///         else: dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    ///dp[i][j] is the longest subequence of text1[0 : i] and text2[0 : j]
+    ///dp[i][j] if text1[i] == text2[j]: 
+    ///            dp[i][j] = dp[i-1][j-1] + 1 (the last maximal subsequence + current pair)
+    ///         else: dp[i][j] = max(dp[i - 1][j] (don't use text1[i]),
+    ///                              dp[i][j - 1]) (don't use text2[i])
     ///
     int longestCommonSubsequence(string text1, string text2) {
         int text1Len = text1.length();
@@ -14,6 +16,9 @@ public:
         
         ///First using a dp table, then the code is slightly modified to
         ///use less space.
+        ///Note: additional row and col are added, to avoid row0 and col0 edge cases
+        ///      they can be thought as the empty string case, so the max common
+        ///      subsequence is 0.
         // vector<vector<int>> dp(text1Len + 1, vector<int>(text2Len + 1, 0));
         // for(int rowIdx = 1; rowIdx <= text1Len; rowIdx++){
         //     for(int colIdx = 1; colIdx <= text2Len; colIdx++){
